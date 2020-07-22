be88d73319e384a98f34c5c436f1ca781ca8aee6
kenta.yoshida <yosi-knt@docomo.ne.jp>
2020-07-22 15:07:57 +0000

    kadai-microposts

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.like(micropost)
+    flash[:success] = 'お気に入りリストに追加しました。'
+    redirect_to root_url
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unlike(micropost)
+    flash[:success] = 'お気に入りリストから削除しました。'
+    redirect_to root_url
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,22 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end 
+  
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end 
+  
+  def like?(micropost)
+    self.likes.include?(micropost)
+  end 
 end
+
+
